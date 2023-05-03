eded4106ccb03ebb547be12badd93b4eb5a2d7b9
Sergio Azcona <sergioazcona@Sergios-MacBook-Pro.local>
2023-05-02 22:32:29 -0400

    schema: dumped b/c R7 still associated; test: response high level keys tested

--- spec/services/giphy/giphy_service_spec.rb
+++ spec/services/giphy/giphy_service_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Giphy Service' do
   describe 'Happy Path' do
-    it 'returns a successful response with high level keys of interest' do
+    it 'responds with high level keys: data, pagination, meta' do
       
       phrase = "If I throw a stick, will you leave?"
       json_response = File.read('spec/fixtures/services/giphy/happy_path/full_phrase_response.json')
@@ -12,9 +12,17 @@ RSpec.describe 'Giphy Service' do
 
       response =  JSON.parse(json_response, symbolize_names: true)
       
-      require 'pry';binding.pry
-    
+      expect(response).to be_a(Hash)
+      expect(response.keys).to eq([:data, :pagination, :meta])
+      expect(response[:data]).to be_a(Array)
+
+      expect(response[:pagination]).to be_a(Hash)
+      expect(response[:pagination].keys).to eq([:total_count, :count, :offset])
+      
+      expect(response[:meta]).to be_a(Hash)
+      expect(response[:meta].keys).to eq([:status, :msg, :response_id])
     end
+    # require 'pry';binding.pry
   end
 
   describe 'Sad Path' do
