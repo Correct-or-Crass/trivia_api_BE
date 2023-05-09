b60e0fb97bfa35746f3590a1db955b280b988849
Sergio Azcona <sergioazcona@Sergios-MacBook-Pro.local>
2023-05-09 16:48:16 -0400

    feat: when response.status is 200, symbolize the response.body

--- app/services/giphy_service.rb
+++ app/services/giphy_service.rb
@@ -1,21 +1,27 @@
+require 'faraday'
+
 class GiphyService
   def self.mean_or_nice(phrase)
-    response = conn.get('/v1/gifs/search') do |s|
-      search.params[:api_key] = ENV['giphy_api_key']
-      search.params[:q] = phrase
-      search.params[:rating] = 'r'
+    response = conn.get('/v1/gifs/search') do |search|
+      search.params['api_key'] = "jZM6kQ1cNOOp9eOG9qZcvhpWIu6L0D0w"
+      search.params['q'] = phrase
+      search.params['rating'] = 'r'
     end
-    require 'pry';binding.pry
-    parse_jason(response)
+    # require 'pry';binding.pry
+    if response.status == 200
+      parse_jason(response) 
+    end
 
   end
 
 
   def self.conn
-    Faraday.new('https://api.giphy.com')
+    Faraday.new('https://api.giphy.com') 
   end
 
+
   def self.parse_json(response)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
 
