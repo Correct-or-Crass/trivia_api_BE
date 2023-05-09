b6866140992885e2723c7880888bf773d02961e5
Sergio Azcona <sergioazcona@Sergios-MacBook-Pro.local>
2023-05-09 16:26:00 -0400

    Faraday Gem added to GemFile

--- app/services/giphy_service.rb
+++ app/services/giphy_service.rb
@@ -1,5 +1,23 @@
 class GiphyService
   def self.mean_or_nice(phrase)
+    response = conn.get('/v1/gifs/search') do |s|
+      search.params[:api_key] = ENV['giphy_api_key']
+      search.params[:q] = phrase
+      search.params[:rating] = 'r'
+    end
+    require 'pry';binding.pry
+    parse_jason(response)
+
   end
 
+
+  def self.conn
+    Faraday.new('https://api.giphy.com')
+  end
+
+  def self.parse_json(response)
+  end
 end
+
+
+GiphyService.mean_or_nice('hello')
